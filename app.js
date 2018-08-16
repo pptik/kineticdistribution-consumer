b622255120cf72233e1403e91dd2904f1298216b
fiyyanputra <hafiyyan.jtk10@gmail.com>
2018-08-16 16:20:40 +0700

    update

--- app.js
+++ app.js
@@ -9,23 +9,22 @@ rmq.connect(rmq_config.broker_uri).then(async (conn) => {
 	ch.consume(q.queue, async (msg) => {
 		console.log("getting message.. ");
 		console.log(msg.content.toString());
+		
 		//{"timestamp":1534408271927,"longitude":116.02551637520645,"latitude":-11.67711076225713,"_id":"5b75364f0dfa8131b47fee5c"}
 		try{
 			let data = JSON.parse(msg.content.toString())
+			let message = {id : data._id, status: true, message: "permintaan sedang diproses"}
+			message = JSON.stringify(message);
+			result = await startTask();
+			if(result){
+				console.log(result);
+			}
+			let response = ch.sendToQueue('respon', new Buffer(message));
+			if(response){
+				console.log("response published")
+			}
 		}catch(error){
 			console.log(error)
-			let data = msg.content.toString()
-		}
-		
-		let message = {id : data._id, status: true, message: "permintaan sedang diproses"}
-		message = JSON.stringify(message);
-		let result = await startTask();
-		if(result){
-			console.log(result);
-		}
-		let response = ch.sendToQueue('respon', new Buffer(message));
-		if(response){
-			console.log("response published")
 		}
 	},  { noAck: true })
 	.catch( (err) => {console.log("Connected to broker failed ", "retry... "+error) })
